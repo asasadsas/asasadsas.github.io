e0895d7f66adc3f9cb87dfc23874f4333f9d68a0
Sam <156713862+asasadsas@users.noreply.github.com>
2024-03-05 10:01:57 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import SiteNavigation from './components/SiteNavigation.vue';
+import ModalVue from './components/Baseamodal.vue'
 </script>
 
 <style lang="scss" scoped>
